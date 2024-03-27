efe31222f90951ea7424270558b775a9fc6cd24b
qiq2i <qiq2i@qq.com>
2024-03-27 19:34:19 +0800

    新增main_window.py作为图形化窗口

---
+++ main_window.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import ttk
+import pyperclip  # 需要额外安装pyperclip库，可通过pip install pyperclip命令安装
+from nbt_to_components import transfer  # 导入transfer函数
+
+root = tk.Tk()#创建主窗口
+
+#设置窗口属性
+root.title("物品NBT格式转components格式")#标题
+root.geometry("900x400")#大小
+
+#创建控件
+label = tk.Label(root, text="物品NBT格式转components格式")
+label.grid(row=0, column=1)#grid()布局
+
+# 创建并放置两个输入框
+label_Item_id = tk.Label(root, text="物品ID：")
+label_Item_id.grid(row=1, column=0)
+
+Item_id = tk.Text(root,width=60,height=1,wrap='word')
+Item_id.grid(row=1, column=1)
+Item_id.insert(tk.END, "stone")
+
+label_Item_id_tip1 = tk.Text(root, wrap='word',width=30,height=10)
+label_Item_id_tip1.grid(row=2, column=2)
+label_Item_id_tip1.insert(tk.END, "物品为成书written_book和书与笔writable_book时，必须写物品id。这是由于这两个物品的NBT有相同的部分，而转化为components时要进行区分。")
+label_Item_id_tip1.configure(cursor="arrow")  # 隐藏光标
+label_Item_id_tip1.config(highlightthickness=0)  # 隐藏高亮边框
+label_Item_id_tip1.config(selectbackground=root.cget("bg"))  # 设置选中文本背景色与窗口背景色一致
+
+label_Item_nbt = tk.Label(root, text="物品NBT：")
+label_Item_nbt.grid(row=2, column=0)
+
+Item_nbt = tk.Text(root,width=60,height=10)
+Item_nbt.grid(row=2, column=1)
+
+# 输出
+label_Item_id = tk.Label(root, text="物品components：")
+label_Item_id.grid(row=4, column=0)
+Item_components = tk.Text(root,width=60,height=10)
+Item_components.grid(row=4, column=1)
+Item_components.insert(tk.END, "[]")
+# 点击转化
+def transfer_bottom():
+    try:
+        id = Item_id.get('1.0', 'end')
+    except Exception:
+        id = "air"
+    try:
+        nbt = Item_nbt.get('1.0', 'end')
+    except Exception:
+        nbt = "{}"
+    try:
+        components = transfer(id,nbt)
+    except Exception:
+        components = "[]"
+    Item_components.delete('1.0', 'end')  # 删除现有内容
+    Item_components.insert(tk.END, components)  # 插入新内容
+
+# 创建一个按钮，点击后获取两个输入框的内容
+get_inputs_button = ttk.Button(root, text="NBT转components", command=transfer_bottom)
+get_inputs_button.grid(row=3, column=1)
+
+def copy_string_to_clipboard():
+    # 需要复制的字符串
+    target_string = Item_components.get('1.0', 'end')
+
+    # 将字符串复制到剪贴板
+    pyperclip.copy(target_string)
+
+copy_button = ttk.Button(root, text="复制组件components", command=copy_string_to_clipboard)
+copy_button.grid(row=5, column=1)
+
+def open_url(url):
+    import webbrowser
+    webbrowser.open_new(url)
+
+# 创建一个Label，其中包含网址文本，并绑定点击事件
+link_label_1 = tk.Label(root, text="点击进入:B站 https://space.bilibili.com/355336076")
+link_label_1.bind("<Button-1>", lambda event: open_url("https://space.bilibili.com/355336076"))
+link_label_1.grid(row=5, column=2)
+
+link_label_2 = tk.Label(root, text="https://github.com/qiq2i/datapack-upgrader")
+link_label_2.bind("<Button-1>", lambda event: open_url("https://github.com/qiq2i/datapack-upgrader"))
+link_label_2.grid(row=6, column=2)
+
+name = tk.Label(root, text="小棋孜")
+name.grid(row=7, column=2)
+# 主循环，处理事件和保持窗口打开
+root.mainloop()
--- test.py
+++ test.py
@@ -1,56 +1,56 @@
-import nbt_to_components
+from nbt_to_components import transfer
 
 #例子1-可染色物品，及通用NBT
 print("测试1")
 s = '''{CanPlaceOn:["minecraft:ice","minecraft:mud"],CanDestroy:["minecraft:fern","minecraft:lava"],display:{Name:'[{"text":"233","color":"gold","bold":true,"italic":true,"underlined":true,"strikethrough":true,"obfuscated":true},{"text":"666777","font":"6","bold":true}]',Lore:['{"text":"112233","bold":true,"italic":true}'],color:1011},HideFlags:129,CustomModelData:12345678,Enchantments:[{id:"minecraft:protection",lvl:3s},{id:"minecraft:thorns",lvl:11s}],AttributeModifiers:[{AttributeName:"generic.max_health",Name:"generic.max_health",Amount:10,Operation:0,UUID:[I;317531815,114708043,-1774063036,1637657640]},{AttributeName:"generic.knockback_resistance",Name:"generic.knockback_resistance",Amount:23,Operation:1,UUID:[I;-1554795033,723864717,-1969707662,611632905],Slot:"mainhand"}],a:"abc"}'''
 #print(parse_nbt(s))
-print(nbt_to_components.transfer("leather",s))
+print(transfer("leather",s))
 
 #测试2 - 书与笔 和 成书
 print("测试2")
 s = '{pages:["123\n123","213"]}'
 d = '{Trim:{material:"minecraft:gold",pattern:"minecraft:eye"}}'
 #print(parse_nbt(s))
-print(nbt_to_components.transfer("writable_book",s))
+print(transfer("writable_book",s))
 #print(parse_nbt(d))
-print(nbt_to_components.transfer("written_book",d))
+print(transfer("written_book",d))
 
 #测试3 - 磁石指针
 print("测试3")
 s = '{LodestoneDimension:"minecraft:overworld",LodestoneTracked:1b,LodestonePos:{X:1,Y:2,Z:3}}'
-print(nbt_to_components.transfer("compass",s))
+print(transfer("compass",s))
 
 #测试4 - 烟火之星
 print("测试4")
 s = '{Explosion:{Type:1,Flicker:1b,Trail:1b,Colors:[I;5057023],FadeColors:[I;9895769]}}'
-print(nbt_to_components.transfer("firework_star",s))
+print(transfer("firework_star",s))
 
 #测试5 - 烟花火箭
 print("测试5")
 s = '{Fireworks:{Flight:10b,Explosions:[{Type:1,Flicker:1b,Trail:1b,Colors:[I;7553279],FadeColors:[I;16770503]},{Type:0,Trail:1b}]}}'
-print(nbt_to_components.transfer("firework_rocket",s))
+print(transfer("firework_rocket",s))
 
 #测试6 - 头颅
 print("测试6")
 s = '{SkullOwner:"xiao_qi_zi",BlockEntityTag:{note_block_sound:"minecraft:ambient.cave"}}'
-print(nbt_to_components.transfer("player_head",s))
+print(transfer("player_head",s))
 
 #测试7 - 陶罐
 print("测试7")
 s = '{BlockEntityTag:{sherds:["minecraft:archer_pottery_sherd","minecraft:brick","minecraft:brick","minecraft:brick"]}}'
-print(nbt_to_components.transfer("decorated_pot",s))
+print(transfer("decorated_pot",s))
 
 #测试8 - 弩
 print("测试8")
 s = '{RepairCost:233,Unbreakable:1b,Damage:3,ChargedProjectiles:[{id:"minecraft:firework_rocket",Count:1b,tag:{Fireworks:{Flight:233b,Explosions:[{Type:0}]}}},{id:"minecraft:arrow",Count:1b},{}],Charged:1b}'
-print(nbt_to_components.transfer("crossbow",s))
+print(transfer("crossbow",s))
 
 #测试9 - 收纳袋
 print("测试9")
 s = '''{Items:[{id:"minecraft:bow",Count:1b,tag:{Damage:233}},{id:"minecraft:egg",Count:3b,tag:{display:{Name:'{"text":"nihao"}'}}}]}'''
-print(nbt_to_components.transfer("bundle",s))
+print(transfer("bundle",s))
 
 #测试10 - 箱子
 print("测试10")
 s = '''{BlockEntityTag:{Items:[{Slot:2b,id:"minecraft:bow",Count:1b,tag:{Damage:3}},{Slot:9b,id:"minecraft:egg",Count:12b}]}}'''
-print(nbt_to_components.transfer("chest",s))
+print(transfer("chest",s))
