78fa43c91b445db60047a27defb146bee539891c
qiq2i <qiq2i@qq.com>
2024-04-05 17:37:48 +0800

    战利品表更新文件测试

---
+++ test_loot.py
@@ -0,0 +1,6 @@
+import loot_update
+
+input_folder_address = 'D:\Minecraft\Minecraft\数据包\我的新创作\时间追逐生存挑战工作区间WorldChallenge\WorldChallenge\数据包\[1.10] MC1.20-1.20.1\data\qiq2i_clga1\loot_tables'
+output_folder_address = 'loot_updata/test'
+
+loot_update.process_json_files(input_folder_address, output_folder_address)
